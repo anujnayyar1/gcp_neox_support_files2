d638a334dbed9e8f8d072b8ea7d88730401e9660
anujnayyar1 <anuj_n@hotmail.co.uk>
2022-06-09 20:16:36 +0100

    Update new_arguments.py

--- new_arguments.py
+++ new_arguments.py
@@ -353,6 +353,8 @@ class NeoXArgs(*BASE_CLASSES):
         )
 
         args_parsed, _ = parser.parse_known_args()
+        print(args_parsed.megatron_config)
+        print(type(args_parsed.megatron_config))
         megatron_config = json.loads(args_parsed.megatron_config)
         if overwrite_values is not None:
             megatron_config.update(overwrite_values)
